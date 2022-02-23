0d479f97680462e50163d3fc5e03db77a9d3e869
vivid <vivid@D-I-U21.04>
2022-02-23 14:42:28 -0500

    updated the css and responsiveness of the details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { format } from 'date-fns';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import '../css/Details.scss';
 
 const Details = () => {
+    const navigate = useNavigate();
     const person = useLocation().state.person;
     let dobDate = format(new Date(person.dob.date), "dd-MM-yyyy");
 
@@ -14,24 +15,35 @@ const Details = () => {
         person[name] = value;
         // onChange(name, value);
     }
+
+    const goBack = () => {
+        navigate(-1);
+    }
     return (
-        <form className='details-container' data-class="details-container" >
-            <div className='details-img-container' data-class="details-img-container">
-                <img className='details profile-img' data-class="profile-img" alt="" src={person.picture.large} />
+        <>
+        <div className="details-main">
+            <p className='back-btn' onClick={goBack}>Back to List</p>
+            <div className='details'>
+                <form className='details-container' data-class="details-container" >
+                    <div className='details-img-container' data-class="details-img-container">
+                        <img className='detail profile-img' data-class="profile-img" alt="" src={person.picture.large} />
+                    </div>
+                    <div className='details-container'>
+                        <input onChange={handleChange} type="text" className='detail' data-class="details-full_name" name="full_name" value={person.full_name} disabled />
+                    </div>
+                    <div className='details-container'>
+                        <input onChange={handleChange} type="tel" className='detail' data-class="details-phone" name="phone" value={person.phone} disabled />
+                    </div>
+                    <div className='details-container'>
+                        <input onChange={handleChange} type="text" className='detail' data-class="details-email" name="email" value={person.email} disabled />
+                    </div>
+                    <div className='details-container'>
+                        <input onChange={handleChange} type="text" className='detail' data-class="details-dob" name="dob" value={dobDate} disabled />
+                    </div>
+                </form>
             </div>
-            <div className='details-container'>
-                <input onChange={handleChange} type="text" className='details' data-class="details-full_name" name="full_name" value={person.full_name} disabled />
-            </div>
-            <div className='details-container'>
-                <input onChange={handleChange} type="tel" className='details' data-class="details-phone" name="phone" value={person.phone} disabled />
-            </div>
-            <div className='details-container'>
-                <input onChange={handleChange} type="text" className='details' data-class="details-email" name="email" value={person.email} disabled />
-            </div>
-            <div className='details-container'>
-                <input onChange={handleChange} type="text" className='details' data-class="details-dob" name="dob" value={dobDate} disabled />
-            </div>
-        </form>
+        </div>
+        </>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,19 +8,25 @@ export default function Home() {
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-    fetch("https://randomuser.me/api/?results=50&inc=name,phone,email,dob,phone,picture")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setContacts(result.results);
-        },
-        (error) => {
-          setIsLoaded(true);
-          alert(error);
-        }
-      )
-  }, []);
+    if(!localStorage.getItem('contacts')) {
+      fetch("https://randomuser.me/api/?results=50&inc=name,phone,email,dob,phone,picture")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setContacts(result.results);
+            localStorage.setItem('contacts', JSON.stringify(result.results));
+          },
+          (error) => {
+            setIsLoaded(true);
+            alert(error);
+          }
+        )
+    } else {
+      setContacts(JSON.parse(localStorage.getItem('contacts')));
+    }
+  }
+  , []);
 
   return (
     <>
